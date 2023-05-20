e61e265befe5f2de642db8330a49645a4f5d7767
Ahad Hamirani <43320587+Ahad48@users.noreply.github.com>
2023-05-20 07:33:20 +0000

    initial python commit

---
+++ scr_py/processing.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import openai
+
+# response = openai.Image.create(
+#   prompt="a white siamese cat",
+#   n=1,
+#   size="1024x1024"
+# )
+# image_url = response['data'][0]['url']
+# print(image_url)
